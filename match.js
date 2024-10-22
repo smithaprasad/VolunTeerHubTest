4cd1a6c5c36648e2369acea5201626c40c5faf31
smithaprasad <smitha.prasad@sourcepulse.com>
2024-10-22 09:32:25 -0500

    Update match.js

--- match.js
+++ match.js
@@ -1,9 +1,9 @@
-const form = document.getElementById('VolunteerForm');
+const form = document.getElementById('volunteerForm');
 
   form.addEventListener('submit', function(event) {
     event.preventDefault(); // Prevent form submission
 
     const formData = new FormData(form);
-    const nameInput = formData.get('name');
-    console.log(nameInput);
+    const locationInput = formData.get('location');
+    console.log(locationInput);
   });
