4ac4aeb6a76be65833f2ae805aaf1521d09fbfb6
smithaprasad <smitha.prasad@sourcepulse.com>
2024-10-22 09:21:33 -0500

    Update match.js

--- match.js
+++ match.js
@@ -1,4 +1,4 @@
-document.getElementById('myForm').addEventListener('submit', function(event) {
+document.getElementById('volunteerForm').addEventListener('submit', function(event) {
             event.preventDefault(); // Prevent the default form submission
             console.log('Form submitted!');
             // Here you can add your custom logic, like sending data to a server
